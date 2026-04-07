d2e353cc73b8bf2a0c3fb062a663ca3206983e84
ahanafy41 <ahmedhanafy884@gmail.com>
2026-04-07 22:27:06 +0200

    Revert "feat(doc_reader): add background playback, pagination, and what's new announcer"

--- fix_home_receiver_3.py
+++
@@ -1,7 +0,0 @@
-with open("main.lua", "r") as f:
-    content = f.read()
-
-content = content.replace("local isListening = false", "local isListening = false\nlocal homeButtonReceiver = nil\nlocal isDocumentReaderBackgrounded = false\nlocal globalDocViewerPath = nil\n")
-
-with open("main.lua", "w") as f:
-    f.write(content)
